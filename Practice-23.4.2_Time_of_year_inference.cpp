d2396d7a3ecd883a92c4c639f2704d42f9800929
Alexandr <sasha_stepanov94@mail.ru>
2024-07-30 16:18:25 +0300

    Second

--- Practice-23.4.2_Time_of_year_inference.cpp
+++ Practice-23.4.2_Time_of_year_inference.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 
-int main (){
-    std::cout << "Fuck off";
+//#define SPRING
+//#define SUMMER
+//#define AUTUMN
+#define WINTER
+
+#ifdef SPRING
+int main () {
+    std::cout << "SPRING";
 }
+#endif
+
+#ifdef SUMMER
+int main () {
+    std::cout << "SUMMER";
+}
+#endif
+
+#ifdef AUTUMN
+int main () {
+    std::cout << "AUTUMN";
+}
+#endif
+
+#ifdef WINTER
+int main () {
+    std::cout << "WINTER";
+}
+#endif
